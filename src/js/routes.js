b0ec3da132ba0fcaaa2fa61b14a3671cc0b0f3ae
lorenzorizzolo <lolloriz36@gmail.com>
2024-10-27 18:22:40 +0100

    ok

--- src/js/routes.js
+++ src/js/routes.js
@@ -5,24 +5,28 @@ import NotFoundPage from '../pages/404.svelte';
 import WebDev from "../pages/web_dev.svelte"
 // studio
 import Studio from "../pages/studio.svelte"
+import { Icon } from 'framework7-svelte';
 
 var routes = [
   {
     path: '/',
     component: HomePage,
     name:"Home Page",
+    icon:"home",
     visible:true
   },
   {
     path: '/web_dev/',
     component: WebDev,
     name:"Web Developer",
+    icon:"language",
     visible:true
   },
   {
     path: '/studio/',
     component: Studio,
     name:"Studio",
+    icon:"school",
     visible: true
   },
   {
--- src/pages/web_dev.svelte
+++ src/pages/web_dev.svelte
@@ -20,14 +20,29 @@
 
     <Block strong inset class="web_info">
       <BlockTitle>Chi è un Web Developer ?</BlockTitle>
-      Un Web Develper è uno sviluppatore di siti online che fornisce esperienze interattive agli utenti e 
-      fornisce delle informazioni agli utenti che visualizzano la pagina
+      Un Web Develper è uno sviluppatore di siti online che realizza pagine web in grado di fornire informazioni agli utenti che visualizzano le pagine. <br>
+      Esistono diverse tipologie di web developer e di progetti realizzabili.
     </Block>
 
     <Block strong inset class="web_info">
       <BlockTitle>Di cosa mi occupo ?</BlockTitle>
+      In particolare la mia attività mira alla realizzazione di gestionali, completamente online, per aziende o attività professionali. Realizziamo anche siti di 
+      presentazione come Curriculum, presentazione di un'azienda o di un negozio.
     </Block>
 
+  </Block>
+
+  <Block strong inset> <h2>Progetti realizzati</h2></Block>
+
+  <Block class={f7.device.desktop ? "grid grid-cols-2 grid-gap": ""}>
+    {#each progetti as p}
+      <Block strong inset class="web_info">
+        <h3>{p.titolo}</h3>
+        {p.desc}
+        <br>Tempo di realizzazione: <span class="tempo">{p.tempo_h} H</span>
+        <br><Link target="_blank" noLinkClass external={true} href={p.link}>{p.link}</Link>
+      </Block>
+    {/each}
   </Block>
 
 
@@ -38,16 +53,45 @@
     Navbar,
     NavLeft,
     NavTitle,
-    NavTitleLarge,
-    NavRight,
     Link,
-    Icon,
-    Toolbar,
     Block,
     BlockTitle,
-    List,
-    ListItem,
-    Button,
     f7
   } from 'framework7-svelte';
+  let progetti = [
+    {
+      titolo:"FileHub",
+      desc:"FileHub è un gestionale online che ti permette l'archiviazione organizzata di documenti per privati e aziende.",
+      tempo_h:"00",
+      link:"https://filehub.rizzolo.cloud"
+    },
+    {
+      titolo:"DocStore",
+      desc:"Gestionale online che ti permette l'archiviazione organizzata di documenti per le aziende, ideato per gli impianti elettrici secondo le norme del DM37/08.",
+      tempo_h:"00",
+      link:"https://docstore.rizzoloimpianti.it"
+    },
+    {
+      titolo:"NodePages",
+      desc:"Ispirato ad Apple Pages è un applicazione web per prendere appunti e gestire le note.",
+      tempo_h:"00",
+      link:"https://nodepages.com"
+    },
+    {
+      titolo:"Global QrCode",
+      desc:"Applicazione web per la generazione di QrCode online associabili a foto, video e link.",
+      tempo_h:"00",
+      link:"https://qrcode.rizzolo.cloud"
+    }
+  ]
 </script>
+
+<style>
+  h3{
+    margin: 0;
+    color: rgb(42, 159, 255);
+  }
+  .tempo{
+    color: orange;
+  }
+</style>
